545697982b6678fed708c7a8f19d9db1696c9343
Patricio2308 <patricioandres00@gmail.com>
2023-09-04 23:24:46 -0300

    creación de Modelo Reserva y boton de salida del registro huesped arreglado

---
+++ src/modelo/Reserva.java
@@ -0,0 +1,66 @@
+package modelo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Reserva {
+    private Integer id;
+    private Date fechaEntrada;
+    private Date fechaSalida;
+    private BigDecimal valor;
+    private String formaDePago;
+
+    public Reserva(Date fechaEntrada, Date fechaSalida, BigDecimal valor, String formaDePago) {
+        this.fechaEntrada = fechaEntrada;
+        this.fechaSalida = fechaSalida;
+        this.valor = valor;
+        this.formaDePago = formaDePago;
+    }
+    public Reserva(){
+
+    }
+
+    public int calcular(){
+        return 0;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(Date fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getFormaDePago() {
+        return formaDePago;
+    }
+
+    public void setFormaDePago(String formaDePago) {
+        this.formaDePago = formaDePago;
+    }
+}
